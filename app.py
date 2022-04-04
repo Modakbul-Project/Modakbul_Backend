0c9fa7f0b98e2adfb06187de46d6d9e34cd24b86
khjung98 <57788781+khjung98@users.noreply.github.com>
2022-04-04 13:29:28 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,41 +1,91 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, redirect, request, url_for
 from pymongo import MongoClient
+
+# db 연동
+conn = MongoClient('127.0.0.1')
+
+# db 생성
+db = conn.Test
+
+# collection 생성
+collect = db.mongoKakao
 
 app = Flask(__name__)
 
-# HTML을 주는 부분
-@app.route('/')
-def home():
-    return render_template('index.html')
 
-@app.route('/manage')
-def manage():
-    return render_template('manage.html')
-
-@app.route('/meeting')
-def meeting():
-    return render_template('meeting.html')
-
-@app.route('/myPage')
-def myPage():
-    return render_template('myPage.html')
+@app.route('/mypage')
+def my_page():
+    return 'This is My Page'
 
 
-@app.route('/join', methods=['POST'])
-def join():
-    data = request.get_json()
-    print(data)
-
-    return jsonify(result = "success", result2= data)
+@app.route("/")
+@app.route('/<int:num>')
+def inputTest(num=None):
+    return render_template('main.html', num=num)
 
 
-@app.route('/member', methods=['POST'])
-def member():
-    data = request.get_json()
-    print(data)
+@app.route('/calculate', methods=['POST'])
+def calculate(num=None):
+    if request.method == 'POST':
+        temp = request.form['num']
+    else:
+        temp = None
+    return redirect(url_for('inputTest', num=temp))
 
-    return jsonify(result = "success", result2= data)
+
+@app.route('/mongo', methods=['GET'])
+def mongoTest():
+    results = collect.find()
+    return render_template('mongo.html', data=results)
+
+
+@app.route('/write', methods=['GET', 'POST'])
+def write():
+    if request.method == "POST":
+        name = request.form["name"]
+        contents = request.form["contents"]
+
+        # document 생성
+        doc = {
+            "name": name,
+            "contents": contents
+        }
+        # document 삽입
+        collect.insert_one(doc)
+        return redirect(url_for('write'))
+    else:
+        return render_template('write.html')
+
+
+@app.route('/kakao')
+def kakao():
+    return render_template('kakao.html')
+
+
+@app.route('/kakao2db', methods=['GET', 'POST'])
+def kakao2db():
+    if request.method == "POST":
+        address = request.form["address"]
+        lat = request.form["lat"]
+        lng = request.form["lng"]
+
+        # document 생성
+        doc = {
+            "address": address,
+            "lat": lat,
+            "lng": lng
+        }
+        # document 삽입
+        collect.insert_one(doc)
+        return redirect(url_for('kakao2db'))
+    else:
+        return render_template('kakao2db.html')
+
+
+@app.route('/kakao_test')
+def kakao_test():
+    return render_template('kakao_test.html')
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run(debug=True)
