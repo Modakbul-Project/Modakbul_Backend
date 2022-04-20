9830d6076af5cffba510671d47198020f4126d68
LivTid <64223268+minune98@users.noreply.github.com>
2022-04-20 18:35:07 +0900

    Delete db_practice.py

--- db_practice.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient('localhost', 27017)
-db = client.users
-
-# db 테스트
-db.users.insert_one({'name': '홍길동', 'gender': 'M', 'email':'hong@google.com'})
