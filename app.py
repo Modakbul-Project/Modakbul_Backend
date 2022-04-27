12f82fccceec61e373bd4379dfe2dcea55bfe625
khjung98 <57788781+khjung98@users.noreply.github.com>
2022-04-27 17:10:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from flask import Flask, render_template, redirect, request, url_for, json, sess
 from pymongo import MongoClient
 from bson.json_util import ObjectId
 from authlib.integrations.flask_client import OAuth
-from markupsafe import escape
-from datetime import timedelta
+from datetime import timedelta, datetime
+
+now = datetime.now()
 
 app = Flask(__name__)
 app.secret_key = 'secretkey'  # secret_key는 서버상에 동작하는 어플리케이션 구분하기 위해 사용하고 복잡하게 만들어야 합니다.
@@ -11,7 +12,7 @@ app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(minutes=60) # 로그인 지
 
 oauth = OAuth(app)
 with open('./static/client_secret.json') as f:
-    json_data=json.load(f)
+    json_data = json.load(f)
 
 
 # json 시리얼 오류 처리용 인코더
@@ -38,71 +39,6 @@ def main():
     # select 쿼리값 results에 저장
     results = collect.find()
     return render_template('main.html', data=results)
-
-
-@app.route('/notice/<id>') #게시판 or 공지사항에 작성된 글 읽기 페이지
-def notice(id=None):
-    if 'userid' in session:  # 로그인 여부 확인
-        # collection 생성
-        collect = db.mongoBoard
-        #db에서 id와 일치하는 글 검색
-        result = list(collect.find({'_id': ObjectId(id)}))
-        result = result[0]
-        return render_template('read.html', data=result)
-    else: #로그인 안되어 있을 경우
-        return redirect('/login')
-
-
-@app.route('/write', methods=['GET', 'POST']) #게시판 or 공지사항 글쓰기 페이지
-def write():
-    if 'userid' in session:  # 로그인 여부 확인
-        if request.method == "POST":
-            # collection 생성
-            collect = db.mongoBoard
-
-            # form에서 가져온 데이터들
-            notice = request.form["notice"]
-            title = request.form["title"]
-            contents = request.form["contents"]
-            userid = session['userid']
-            username = session['username']
-
-            # document 생성
-            doc = {
-                "notice": notice,
-                "title": title,
-                "contents": contents,
-                "userid": userid,
-                "username": username,
-                "create_time": str(now.date())
-            }
-            collect.insert_one(doc)
-            return redirect(url_for('meet_page'))
-        # GET일 경우
-        return render_template('write.html')
-    else: #로그인 안되어 있을 경우
-        return redirect('/login')
-
-
-@app.route('/meet')
-def meet_page():
-    if 'userid' in session:  # 로그인 여부 확인
-        # collection 생성
-        collect = db.mongoBoard
-
-        # select 쿼리값 results에 저장
-        results = collect.find()
-        return render_template('meetpage.html', data=results)
-    else:
-        return redirect('/login')
-
-
-@app.route('/meetadmin')
-def meet_admin():
-    if 'userid' in session:  # 로그인 여부 확인
-        return render_template('meetpage.html')
-    else:
-        return redirect('/login')
 
 
 @app.route('/find_pw', methods=['GET', 'POST'])
@@ -233,7 +169,6 @@ def login():
 def logout():
     #세션에서 값 삭제
     session.pop('userid', None)
-
     return redirect(url_for('main'))
 
 
@@ -279,6 +214,131 @@ def signup():
             return redirect(url_for('signup'))
     else:  # 메소드가 GET일 경우
         return render_template('signup.html')
+
+
+@app.route('/notice/<id>') #게시판 or 공지사항에 작성된 글 읽기 페이지
+def notice(id=None):
+    if 'userid' in session:  # 로그인 여부 확인
+        # collection 생성
+        collect = db.mongoBoard
+        #db에서 id와 일치하는 글 검색
+        result = list(collect.find({'_id': ObjectId(id)}))
+        result = result[0]
+        return render_template('read.html', data=result)
+    else: #로그인 안되어 있을 경우
+        return redirect('/login')
+
+
+@app.route('/write', methods=['GET', 'POST']) #게시판 or 공지사항 글쓰기 페이지
+def write():
+    if 'userid' in session:  # 로그인 여부 확인
+        if request.method == "POST":
+            # collection 생성
+            collect = db.mongoBoard
+
+            # form에서 가져온 데이터들
+            notice = request.form["notice"]
+            title = request.form["title"]
+            contents = request.form["contents"]
+            userid = session['userid']
+            username = session['username']
+
+            # document 생성
+            doc = {
+                "notice": notice,
+                "title": title,
+                "contents": contents,
+                "userid": userid,
+                "username": username,
+                "create_time": str(now.date())
+            }
+            collect.insert_one(doc)
+            return redirect(url_for('meet_page'))
+        # GET일 경우
+        return render_template('write.html')
+    else: #로그인 안되어 있을 경우
+        return redirect('/login')
+
+
+@app.route('/meet')
+def meet_page():
+    if 'userid' in session:  # 로그인 여부 확인
+        # collection 생성
+        collect = db.mongoBoard
+
+        # select 쿼리값 results에 저장
+        results = collect.find()
+        return render_template('meetpage.html', data=results)
+    else:
+        return redirect('/login')
+
+
+@app.route('/meetadmin')
+def meet_admin():
+    if 'userid' in session:  # 로그인 여부 확인
+        return render_template('meetpage.html')
+    else:
+        return redirect('/login')
+
+
+@app.route('/delete/<id>')
+def delete(id=None):
+    if 'userid' in session:  # 로그인 여부 확인
+        # collection 생성
+        collect = db.mongoBoard
+        #db에서 id와 일치하는 글 검색
+        result = list(collect.find({'_id': ObjectId(id)}))
+        if result[0]['userid'] == session['userid']: #작성자와 로그인한 사용자가 일치하면
+            collect.delete_one({'_id': ObjectId(id)}) #해당 게시글 삭제
+            return redirect(url_for('meet_page'))
+        else: #작성자와 로그인한 사용자가 일치하지 않으면
+            msg = "삭제 권한이 없습니다."
+            flash(msg)  # 리턴할 때 같이 넘겨줄 메시지
+            return redirect('/notice/'+id)
+    else:  # 로그인 안되어 있을 경우
+        return redirect('/login')
+
+
+@app.route('/edit/<id>', methods=['GET', 'POST']) #게시판 or 공지사항에 작성된 글 읽기 페이지
+def edit(id=None):
+    if 'userid' in session:  # 로그인 여부 확인
+        # collection 생성
+        collect = db.mongoBoard
+
+        if request.method == "POST":
+
+            # form에서 가져온 데이터들
+            notice = request.form["notice"]
+            title = request.form["title"]
+            contents = request.form["contents"]
+            userid = session['userid']
+            username = session['username']
+
+            # document 생성
+            doc = {
+                "notice": notice,
+                "title": title,
+                "contents": contents,
+                "userid": userid,
+                "username": username,
+                "create_time": str(now.date())
+            }
+            collect.update_one({'_id': ObjectId(id)}, {'$set': doc})
+            return redirect(url_for('meet_page'))
+
+        # GET일 경우
+        #db에서 id와 일치하는 글 검색
+        result = list(collect.find({'_id': ObjectId(id)}))
+        if result[0]['userid'] == session['userid']: #작성자와 로그인한 사용자가 일치하면
+            result = result[0]
+            return render_template('edit.html', data=result)
+        else: #작성자와 로그인한 사용자가 일치하지 않으면
+            msg = "수정 권한이 없습니다."
+            flash(msg)  # 리턴할 때 같이 넘겨줄 메시지
+            return redirect('/notice/'+id)
+
+    else: #로그인 안되어 있을 경우
+        return redirect('/login')
 
 
 if __name__ == '__main__':
